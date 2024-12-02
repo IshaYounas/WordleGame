7ad351e04755ee72c2d0e655031833d34659a269
ISHA YOUNAS <G00427804@atu.ie>
2024-12-02 19:33:36 +0000

    Add project files.

---
+++ MyWordleGame/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWordleGame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyWordleGame/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWordleGame
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
