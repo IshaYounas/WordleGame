fa6451cf16d8ac782d9cee15962fec217df521c2
ISHA YOUNAS <G00427804@atu.ie>
2024-12-16 18:36:50 +0000

    Created Static Resource Styles for the buttons/label(s). Also created event handlers for buttons

--- MyWordleGame/MainPage.xaml.cs
+++ MyWordleGame/MainPage.xaml.cs
@@ -6,8 +6,22 @@ namespace MyWordleGame
         public MainPage()
         {
             InitializeComponent();
-        }
+        } // MainPage
 
-    }
+        // methods for buttons
+        private void Key_Clicked(object sender, EventArgs e)
+        {
 
-}
+        } // Key_Clicked
+
+        private void Enter_Clicked(object sender, EventArgs e)
+        {
+
+        } // Enter_Clicked
+
+        private void Del_Clicked(object sender, EventArgs e)
+        {
+
+        } // Del_Clicked
+    } // class
+} // namespace
