168352090317a63a2d9cfc44b3ac24b8f678ba78
ISHA YOUNAS <G00427804@atu.ie>
2024-12-23 15:38:18 +0000

    Calling the file and working on its methods

--- MyWordleGame/MainPage.xaml.cs
+++ MyWordleGame/MainPage.xaml.cs
@@ -4,6 +4,10 @@ namespace MyWordleGame
     public partial class MainPage : ContentPage
     {
         // fields
+        private const string LocalFile = "words.txt";
+        private const string OnlineFile = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt";
+        private List<string> list = new List<string>();
+        private string targetWord = "";
         private string currentGuess = "";
         private const int maxRows = 6;
         private const int maxCols = 5;
@@ -15,6 +19,7 @@ namespace MyWordleGame
             InitializeComponent();
 
             // calling custom methods
+            InitializeList();
             CreateGameGrid();
         } // MainPage
 
@@ -22,6 +27,7 @@ namespace MyWordleGame
         private void Key_Clicked(object sender, EventArgs e)
         {
             var button = sender as Button;
+
             if (button != null && currentGuess.Length < maxCols)
             {
                 currentGuess += button.Text; // adding the guess letter to the input box
@@ -29,9 +35,40 @@ namespace MyWordleGame
             } // if
         } // Key_Clicked
 
-        private void Enter_Clicked(object sender, EventArgs e)
+        private async void Enter_Clicked(object sender, EventArgs e)
         {
-            // enter button yet to be completed
+            var label = (Label)GameGrid.Children[currentRow * maxCols + 1];
+
+            if (currentGuess.Length == maxCols && currentRow < maxRows)
+            {
+                // if the word is not in the list
+                if (!list.Contains(currentGuess))
+                {
+                    await DisplayAlert("Invalid Word", "Word Not Found", "Ok");
+                    return;
+                } // if
+
+                // if the word is the target word
+                if (currentGuess == targetWord)
+                {
+                    await DisplayAlert("Congrats", "You guessed the correct word!", "Ok");
+                    return;
+                } // if
+
+                for (int i = 0; i < maxCols; i++)
+                {
+                    // setting the colours as it is in  online game
+
+                    if (currentGuess[i] == targetWord[i])
+                        label.BackgroundColor = Colors.Green; // if
+
+                    else if (targetWord.Contains(currentGuess[i]))
+                        label.BackgroundColor = Colors.Yellow; // else if
+
+                    else
+                        label.BackgroundColor = Colors.Gray; 
+                } // for
+            } // if
         } // Enter_Clicked
 
         private void Del_Clicked(object sender, EventArgs e)
@@ -53,6 +90,7 @@ namespace MyWordleGame
                 {
                     var label = new Label
                     {
+                        // setting the properties for the grid
                         BackgroundColor = Colors.AntiqueWhite,
                         FontSize = 20,
                         TextColor = Colors.DarkBlue,
@@ -87,5 +125,68 @@ namespace MyWordleGame
                     label.Text = ""; // else
             } // for
         } // UpdateGuessDisplay
+
+        private async void InitializeList()
+        {
+            string localPath = Path.Combine(FileSystem.AppDataDirectory, LocalFile); // getting the full path for the local file
+
+            // calling the appropriate methods
+            // reading the words from the file
+            if (File.Exists(localPath))
+                list = await ReadFromFile(localPath); // if
+
+            else
+            { 
+                // downloading the file to the local path
+                await DownloadFromOnline(localPath);
+                list = await ReadFromFile(localPath);
+            } // else
+
+            // randomly selecting a word for the current game
+            if (list.Count > 0)
+            {
+                Random word = new Random();
+                targetWord = list[word.Next(list.Count)];
+            } // if
+        } // InitializeList
+
+        private async Task DownloadFromOnline(string localPath)
+        {
+            using (HttpClient client = new HttpClient()) // getting the word for the word list
+            {
+                try
+                {
+                    // throwing an exception if the the http request fails
+                    var response = await client.GetAsync(OnlineFile);
+                    response.EnsureSuccessStatusCode(); 
+
+                    // writing the downloaded list to the file 
+                    var wordsOnline = await response.Content.ReadAsStringAsync();
+                    await File.WriteAllTextAsync(localPath, wordsOnline);
+
+                } // try
+
+                catch (Exception ex) // catching the exception
+                {
+                    Console.WriteLine($"Error downloading words: {ex.Message}");
+                } // catch
+            } // HttpClient client
+        } // DownloadFromOnline
+
+        private async Task<List<string>> ReadFromFile(string localPath)
+        {
+            try
+            {
+                var wordsFile = await File.ReadAllTextAsync(localPath); // string
+                // splitting the stings
+                return wordsFile.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList(); 
+            } // try
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading words from file: {ex.Message}");
+                return new List<string>();
+            } // catch
+        } // ReadFromFile
     } // class
 } // namespace
