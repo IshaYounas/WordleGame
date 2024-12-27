79cce16ecefaea8124a532174867930d11fd0a87
ISHA YOUNAS <G00427804@atu.ie>
2024-12-27 21:24:30 +0000

    Added a page for the progess of the attempts

---
+++ MyWordleGame/Progress.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWordleGame
+{
+    public class Progress
+    {
+        public DateTime TimeStamp { get; set; }
+        public string CorrectWord { get; set; }
+        public int Guesses { get; set; }
+        public string Emoji { get; set; }
+    } // class
+} // namespace
