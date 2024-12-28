224f5021a11fd0f1660b39a07a53e2f5c13b9a78
ISHA YOUNAS <G00427804@atu.ie>
2024-12-28 19:00:48 +0000

    Added a Player class and relevant methods to go with it

---
+++ MyWordleGame/Player.cs
@@ -0,0 +1,59 @@
+﻿namespace MyWordleGame
+{
+    public class Player
+    {
+        // variables
+        public string Name { get; set; }
+        public int Score { get; set; }
+        private string filePath => $"{Name}.txt";
+
+        // constructor
+        public Player(string name)
+        {
+            this.Name = name;
+
+            // calling methods
+            if (File.Exists(filePath))
+                LoadPlayer(); // previous player
+
+            else
+                NewPlayer();
+        } // Player
+
+        // custom methods
+        private void LoadPlayer() // loading a saved player
+        {
+            Console.WriteLine($"Welcome back, {Name}!");
+            
+            string data = File.ReadAllText(filePath);
+
+            // replacing the data string with "Score:"
+            if (int.TryParse(data.Replace("Score: ", ""), out int score))  // converting string into an integer
+                Score = score;
+
+            else
+            {
+                Console.WriteLine("Error reading score");
+                Score = 0;
+            } // else
+        } // LoadPlayer
+
+        private void NewPlayer() // creating a new player
+        {
+            Console.Write($"Welcome to the game of Wordle, {Name}");
+            Score = 0;
+            SavePlayer();
+        } // NewPlayer
+
+        public void SavePlayer() // saving to a file
+        {
+            File.WriteAllText(filePath, $"Score: {Score}");
+        } // SavePlayer
+
+        public void UpdateScore(int points) // updating score
+        {
+            Score += points;
+            SavePlayer();
+        } // UpdateScore
+    } // class
+} // namespace
